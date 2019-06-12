2dd4f06921ac485acf3e26a2ce292c9261b8a795
Mathias Jakob <mathias.jakob@ebcsoft.de>
2019-06-12 09:58:48 +0200

    Add uwp project

--- appcenter-issue/MainPage.xaml.cs
+++ appcenter-issue/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+
 namespace appcenter_issue
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
@@ -13,8 +14,10 @@ namespace appcenter_issue
     [DesignTimeVisible(true)]
     public partial class MainPage : ContentPage
     {
+        private string test;
+
         public MainPage()
-        {
+        { 
             InitializeComponent();
         }
     }
